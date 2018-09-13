5b39e659cc579dd6e8ec7f2963072ef5aa59175e
Tommy Kärnström <31956581+tomkaar@users.noreply.github.com>
2018-09-13 19:54:09 +0200

    Moved Check Login on start from App.js to Index.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,6 @@
 import React from "react";
-import { connect } from "react-redux";
-import firebase from "firebase";
-
-import { userLogin, userLogout } from "./actions/user";
 import AppRouter from "./routers/AppRouter";
 
-import "./styles/styles.scss";
-
-class App extends React.Component {
-
-    state = {
-        "loaded": false,
-        firebase: ""
-    }
-
-    componentWillMount() {
-        this.setState(() => ({
-            firebase: firebase.auth()
-                .onAuthStateChanged((user) => {
-                    if (user) {
-                        this.props.userLogin(user);
-                        this.setState(() => ({ loaded: true }));
-                    } else {
-                        this.props.userLogout();
-                        this.setState(() => ({ loaded: true }));
-                    }
-                })
-        }))
-    }
-
-    render() {
-        this.state.loaded === true && this.state.firebase();
-        return <AppRouter />;
-    }
-}
-
-const mapStateToProps = (state) => ({
-    user: state.user
-});
-const mapDispatchToProps = (dispatch) => ({
-    userLogin: (user) => dispatch(userLogin(user)),
-    userLogout: () => dispatch(userLogout())
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
-
-// when the App was mounted, check if user is logged in and update redux state
-// use connect to access to the Redux State
+export default () => (
+    <AppRouter />
+);
--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import "./styles/styles.scss";
+
 import { Provider } from "react-redux";
+import firebase from "firebase";
+
 import store from "./store/store";
 import { startSetRecipe } from './actions/recipes';
+import { userLogin, userLogout } from "./actions/user";
 
 store.subscribe(() => { console.log(store.getState()); });
 
@@ -15,10 +20,20 @@ const app = (
     </Provider>
 );
 
+// Loading screen until app is ready
 ReactDOM.render(<p>Loading..</p>, document.getElementById('root'));
 
+// Get all recipes from firebase
 store.dispatch(startSetRecipe())
+    // Check if user is logged in
     .then(() => {
+        firebase.auth()
+            .onAuthStateChanged((user) => {
+                user ? store.dispatch(userLogin(user)) : store.dispatch(userLogout());
+            });
+    })
+    .then(() => {
+        // When App is Ready, render
         ReactDOM.render(app, document.getElementById('root'));
     });
 
