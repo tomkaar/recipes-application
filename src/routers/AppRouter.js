732ed2cd5308fee27632f45f78dcb6fbad9e4b61
Tommy Kärnström <31956581+tomkaar@users.noreply.github.com>
2018-09-14 01:30:52 +0200

    Heroku test 5

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import RequireAuthentication from "../components/test";
+import RequireAuthentication from "../components/RequireAuthentication";
 import Messages from "../components/Messages";
 
 import Header from "../components/Header";
