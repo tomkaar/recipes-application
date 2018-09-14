209422a9b4e2c0766c6957a1a3a43ea5b9651af6
Tommy Kärnström <31956581+tomkaar@users.noreply.github.com>
2018-09-14 11:09:59 +0200

    Fixed RecipeListFilter

--- src/components/RecipeListItem.js
+++ src/components/RecipeListItem.js
@@ -3,21 +3,28 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { firebaseRemoveRecipe } from '../actions/recipes';
 
-const RecipeListItem = ({ title, description, id, createdBy, timestamp, removeRecipe}) => { 
-    const handleRemove = (e) => {
-        console.log("REMOVE");
-        removeRecipe(id);
-    }
+const RecipeListItem = ({ title, description, id, createdBy, timestamp, removeRecipe, user}) => { 
+    const handleRemove = () => removeRecipe(id);
+
+    const time = new Date(timestamp);
+
     return (
-    <div className="RecipeListItem">
-        <Link to={`/recipe/${id}`}>{title}</Link>
-        <p>{description}</p>
-        <button onClick={handleRemove}>Remove</button>
-    </div>
-)};
+        <div className="RecipeListItem">
+            <Link to={`/recipe/${id}`}>{title}</Link>
+            { user.uid === createdBy &&  <Link to={`/edit/${id}`}> - edit</Link> }
+            <p>{`${time.getDate()}/${time.getMonth()} - ${time.getFullYear()} ${time.getHours()}:${time.getMinutes()}`}</p>
+            <p>{description}</p>
+            <button onClick={handleRemove}>Remove</button>
+        </div>
+    )
+};
+
+const mapStateToProps = (state) => ({
+    user: state.user
+});
 
 const mapDispatchToProps = (dispatch) => ({
     removeRecipe: (id) => dispatch(firebaseRemoveRecipe(id))
 });
 
-export default connect(undefined, mapDispatchToProps)(RecipeListItem);
+export default connect(mapStateToProps, mapDispatchToProps)(RecipeListItem);
