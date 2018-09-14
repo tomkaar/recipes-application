4acd3b830f1df6569075ad13eac3ea084f640ed7
Tommy Kärnström <31956581+tomkaar@users.noreply.github.com>
2018-09-14 20:26:21 +0200

    Messages is now a stateless component

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -2,25 +2,16 @@ import React from "react";
 import { connect } from "react-redux";
 import Message from "./Message"
 
-
-
-class Messages extends React.Component {
-    render() {
-        return (
-            this.props.message.length > 0 && (
-                <div className="messages">
-                    {this.props.message.map(x => <Message key={x.id} {...x} />)}
-                </div>
-            )
-        
+const Messages = (props) => (
+    props.messages.length > 0 && (
+        <div className="messages">
+            {props.messages.reverse().map(message => <Message key={message.id} {...message} />)}
+        </div>
     )
-}
-}
-
-
+)
 
 const mapStateToProps = (state) => ({
-    message: state.messages
+    messages: state.messages
 });
 
 export default connect(mapStateToProps)(Messages);
