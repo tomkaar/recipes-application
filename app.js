c94ac7c24104ea7c6cc350b193533a095b902159
Tommy Kärnström <31956581+tomkaar@users.noreply.github.com>
2018-09-16 16:53:03 +0200

    Heroku Update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const publicPath = path.join(__dirname, "..", "build");
+const publicPath = path.join(__dirname, "build");
 const port = process.env.PORT || 8080;
 
 app.use(express.static(publicPath));
