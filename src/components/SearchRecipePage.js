91f4ca5fe771a4669558ceda60fe54c3c669fbde
Tommy Kärnström <31956581+tomkaar@users.noreply.github.com>
2018-09-17 16:55:06 +0200

    SearchRecipePage Added

---
+++ src/components/SearchRecipePage.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { connect } from "react-redux";
+import { database } from "../firebase/Firebase";
+import { addRecipe, removeRecipe, editRecipe, clearRecipe } from '../actions/recipes';
+import selectRecipes from "../selectors/selectRecipes";
+
+import PageHeader from "./PageHeader";
+import RecipeListFilters from "./RecipeListFilters";
+import RecipeList from "./RecipeList";
+
+class DashboardPage extends React.Component {
+    state = {
+        ref: "",
+    }
+
+    componentDidMount() {
+        this.props.clearRecipes();
+        const ref = database.ref("recipes");
+        this.setState(() => ({ ref }))
+        
+        ref.orderByChild("title")
+            .startAt(this.props.filters.text)
+            .endAt(this.props.filters.text + "\uf8ff")
+            .on("child_added", (snapshot) => {
+                this.props.addRecipe({
+                    ...snapshot.val(), id: snapshot.key
+                })
+        })
+        ref.on("child_removed", snapshot => {
+            this.props.removeRecipe(snapshot.key);
+        })
+        ref.on("child_changed", snapshot => {
+            this.props.editRecipe(snapshot.key, { ...snapshot.val(), id: snapshot.key });
+        })
+    }
+
+    componentWillUnmount() {
+        this.state.ref.off();
+        this.props.clearRecipes();
+    }
+
+    render() {
+        return (
+            <div className="SerachRecipePage">
+                <PageHeader title="Search Recipes" para="Find what you are looking for" />
+                <RecipeListFilters />
+                <RecipeList recipes={selectRecipes(this.props.recipes, this.props.filters)} />
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => ({
+    recipes: state.recipes,
+    filters: state.filters
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    addRecipe: (recipe) => dispatch(addRecipe(recipe)),
+    editRecipe: (id, updated) => dispatch(editRecipe(id, updated)),
+    removeRecipe: (id) => dispatch(removeRecipe(id)),
+    clearRecipes: () => dispatch(clearRecipe())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(DashboardPage);
