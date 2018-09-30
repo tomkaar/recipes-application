d0d8acb455088f1ab6857db6e1455218026b3ed0
Tommy Kärnström <31956581+tomkaar@users.noreply.github.com>
2018-09-30 18:11:48 +0200

    Resposive Navigation menu

--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -7,10 +7,20 @@ import Button from "./NavButton";
 
 class Header extends React.Component {
 
+    state = {
+        active: false
+    }
+
     handleLogout = () => { Logout(); }
     handleLoginNavigationButton = () => {
         console.log("OK");
     };
+
+    toggleActive = () => {
+        this.setState((prevState) => ({
+            active: !prevState.active
+        }))
+    }
 
     render() {
         return (
@@ -20,20 +30,23 @@ class Header extends React.Component {
                         <NavLink to="/" className="navbar-brand navbar-item l-margin-right">
                             <h1>Recipes</h1>
                         </NavLink>
+                    </div>
+                    <div className={`navbar-menu-wrapper ${this.state.active ? "active" : ""}`}>
                         <div className="navbar-menu">
                             <NavLink exact activeClassName="active" className="navbar-item" to="/">Home</NavLink>
                             <NavLink exact activeClassName="active" className="navbar-item" to="/search">Search</NavLink>
                             {this.props.user.user && <NavLink activeClassName="active" className="navbar-item" to="/new">New</NavLink>}
                         </div>
-                    </div>
-                    <div className="navigation__right">
-                        <div className="navbar-menu">
+                        <div className="navbar-auth">
                             {this.props.user.user ?
                                 <button onClick={this.handleLogout} className="Button navbar-button">Logout</button> :
                                 <Button url="/login">Login or Register</Button>
                             }
                         </div>
                     </div>
+                    <button className={`navbar-menu-button ${this.state.active ? "active" : ""}`} onClick={this.toggleActive}>
+                        <span></span> <span></span> <span></span>
+                    </button>
                 </div>
             </nav>
         )
